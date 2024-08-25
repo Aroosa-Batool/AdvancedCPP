22f56441046485f35542c107d3901277ef966257
Aroosa Batool <aroosa.batool@idealo.de>
2024-08-26 01:59:28 +0200

    update example 7

--- basic_statements/example07.cpp
+++ basic_statements/example07.cpp
@@ -1,14 +1,52 @@
 /**
- Write a program that draws in the terminal a bigger X out of the character 'X',  
+ Write a program that draws in the terminal a big X out of the character 'X',
  depending on the variable int size (with size = 3, 4, ..., 20):
  size = 3:     size = 4:      size = 5:      etc.
-   XX X         XX  X          XX   X
-    XX           XXX            XX X
-   X XX          XXX             XX
-                X  XX           X XX
-                               X   XX      */
+   X X          X  X           X   X
+    X            XX             X X
+   X X           XX              X
+                X  X            X X
+                               X   X      */
 #include <iostream>  // to allow use of std::cout and std::endl
+using namespace std;
 int main( ) {
+
+        int input =0;
+        cin>> input;
+
+        int input2 = input-1;
+        int y=0;
+
+
+
+
+        for(int i=0; i<input+1; i++)
+        {
+            int p =y;
+
+           for(int j=0; j<input; j++)
+           {
+                if(p==i || p== input2 ){
+                    cout<< "X";
+                    if(p==i)
+                    {
+                        cout<< "X";
+                    }
+
+                }
+                else{
+                    cout<< " ";
+
+                }
+                 p++;
+
+           }
+           cout<<endl;
+
+
+           input2--;
+
+        }
 
 
 
